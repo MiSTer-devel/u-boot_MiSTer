ef0d403310dcf73df1966f1d2bf8a91db1aff6af
Sorgelig <pour.garbage@gmail.com>
2017-04-01 21:39:09 +0800

    Update environment variables.

--- include/configs/socfpga_de10_nano.h
+++ include/configs/socfpga_de10_nano.h
@@ -19,7 +19,7 @@
 
 /* Booting Linux */
 #define CONFIG_BOOTFILE		"fitImage"
-#define CONFIG_BOOTARGS		"console=ttyS0," __stringify(CONFIG_BAUDRATE)
+#define CONFIG_BOOTARGS		"console=ttyS0," __stringify(CONFIG_BAUDRATE) " mem=512M memmap=512M$512M"
 #define CONFIG_BOOTCOMMAND	"run mmcload; run mmcboot"
 #define CONFIG_LOADADDR		0x01000000
 #define CONFIG_SYS_LOAD_ADDR	CONFIG_LOADADDR
@@ -44,27 +44,25 @@
 #define CONFIG_EXTRA_ENV_SETTINGS \
 	"preboot=usb start\0" \
 	"loadaddr=" __stringify(CONFIG_SYS_LOAD_ADDR) "\0" \
-	"ramboot=setenv bootargs " CONFIG_BOOTARGS ";" \
-		"bootm ${loadaddr} - ${fdt_addr}\0" \
 	"bootimage=zImage\0" \
 	"fdt_addr=100\0" \
 	"fdtimage=socfpga.dtb\0" \
 	"fpgadata=0x02000000\0" \
-	"core=soc_system.rbf\0" \
-	"fpgaload=fatload mmc 0:$mmc_boot $fpgadata $core; fpga load 0 $fpgadata $filesize; bridge enable\0" \
+	"core=init.rbf\0" \
+	"fpgaload=load mmc 0:$mmc_boot $fpgadata $core; fpga load 0 $fpgadata $filesize; bridge enable\0" \
+	"scrload=load mmc 0:${mmc_boot} ${loadaddr} u-boot.scr;source ${loadaddr}\0" \
+	"scrtest=if test -e mmc 0:$mmc_boot /u-boot.scr;then run scrload;fi\0" \
 	"ethaddr=02:03:04:05:06:07\0" \
 	"bootm ${loadaddr} - ${fdt_addr}\0" \
 	"mmc_boot=" __stringify(CONFIG_SYS_MMCSD_FS_BOOT_PARTITION) "\0" \
 	"mmc_os=" __stringify(CONFIG_SYS_MMCSD_FS_OS_PARTITION) "\0" \
 	"mmcroot=/dev/mmcblk0p" __stringify(CONFIG_SYS_MMCSD_FS_OS_PARTITION) "\0" \
-	"mmcboot=setenv bootargs " CONFIG_BOOTARGS \
-		" root=${mmcroot} rw rootwait;" \
-		"bootz ${loadaddr} - ${fdt_addr}\0" \
+	"mmcboot=setenv bootargs " CONFIG_BOOTARGS " root=${mmcroot} rw rootwait;" "bootz ${loadaddr} - ${fdt_addr}\0" \
 	"mmcload=mmc rescan;" \
-		"load mmc 0:${mmc_boot} ${loadaddr} u-boot.scr;" \
-		"source ${loadaddr};" \
-		"load mmc 0:${mmc_boot} ${loadaddr} ${bootimage};" \
-		"load mmc 0:${mmc_boot} ${fdt_addr} ${fdtimage}\0" \
+		"run fpgaload;" \
+		"run scrtest;" \
+		"load mmc 0:${mmc_os} ${loadaddr} /boot/${bootimage};" \
+		"load mmc 0:${mmc_os} ${fdt_addr} /boot/${fdtimage}\0" \
 
 /* The rest of the configuration is shared */
 #include <configs/socfpga_common.h>
